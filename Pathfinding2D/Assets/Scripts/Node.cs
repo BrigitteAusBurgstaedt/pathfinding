ed4bbf5f7969a43e8cfd15073e100fe163403bdc
BrigitteAusBurgstaedt <robin260602@gmail.com>
2023-11-02 15:14:24 +0100

    Add Node

---
+++ Pathfinding2D/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IComparer<Node>
+{
+
+
+    public int Compare(Node x, Node y)
+    {
+        throw new System.NotImplementedException();
+    }
+}
