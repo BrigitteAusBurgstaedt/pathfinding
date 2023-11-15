2fab8ecdfa34cbeb1b302c3b2b13fc633749871d
Tbranke <tbranke@hs-mittweida.de>
2023-11-15 19:49:24 +0100

    quiz code implementation

---
+++ Pathfinding2D/Assets/Code/Scripts/AnswerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerScript : MonoBehaviour
+{
+
+	public bool isCorrect = false;
+	public QuizManager quizmanager;
+
+	void Answer()
+    {
+
+		if (isCorrect)
+		{
+			Debug.Log("Korrekt");
+			quizmanager.correct();
+		}
+		else
+		{
+			Debug.Log("Falsch");
+			quizmanager.correct();
+
+		}
+
+	}
+
+
+}
---
+++ Pathfinding2D/Assets/Code/Scripts/QuestionAndAnswer.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+
+public class QuestionAndAnswer
+{
+    public string Questions;
+    public string[] Answers;
+    public int CorrectAnswer;
+}
