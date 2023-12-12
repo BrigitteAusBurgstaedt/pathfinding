4f6327b37ac60ccba6a3d5051d565f620e663c6a
Tbranke <tbranke@hs-mittweida.de>
2023-12-12 08:22:59 +0100

    Implementierung Farbwechsel der Antworten (1)

--- Pathfinding2D/Assets/Code/Scripts/AnswerScript.cs
+++ Pathfinding2D/Assets/Code/Scripts/AnswerScript.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class AnswerScript : MonoBehaviour
 {
@@ -7,16 +9,25 @@ public class AnswerScript : MonoBehaviour
 	public bool isCorrect = false;
 	public QuizManager quizmanager;
 
-	public void Answer()
+	public Color startColor;
+
+   private void Start()
+    {
+		startColor = GetComponent<Image>().color;
+    }
+
+    public void Answer()
     {
 
 		if(isCorrect)
 		{
+			GetComponent<Image>().color = Color.green;
 			Debug.Log("Korrekt");
 			quizmanager.Correct();
 		}
 		else
 		{
+			GetComponent<Image>().color = Color.red;
 			Debug.Log("Falsch");
 			quizmanager.wrong();
 
