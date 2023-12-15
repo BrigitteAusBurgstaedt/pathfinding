3b17062fafa99c58c8dd4866d65f2124f8deb67d
Jojon10 <jojon10wot@web.de>
2023-12-15 15:36:55 +0100

    Tiefensuche

--- Pathfinding2D/Assets/Code/Scripts/pathfinding/DepthFirst.cs
+++ Pathfinding2D/Assets/Code/Scripts/pathfinding/DepthFirst.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using Unity.Collections;
 using UnityEngine.Tilemaps;
 
 namespace pathfinding
@@ -10,7 +13,27 @@ namespace pathfinding
 
         protected override bool SearchPath(Spot start, Spot end)
         {
-            throw new NotImplementedException();
+            start.Visited = true;
+
+
+                if (start.Equals(end))
+                {
+                    return true;
+                }
+
+                for (int i = 0; i < start.Neighbors.Count; i++)
+                {
+                    if (!start.Neighbors[i].Visited)
+                    {
+                        if (start.Neighbors[i].IsWalkable)
+                        {
+                            SearchPath(start.Neighbors[i], end);
+                        }
+                    }
+                }
+
+            return false;
+
         }
     }
 }
